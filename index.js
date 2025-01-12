0d0070a38e0a3f84a949b1e07e901d16b467938c
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-01-12 15:48:46 -0500

    testing api response

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-import { v4 as uuidv4 } from 'uuid';
 
-console.log(uuidv4());
-console.log("Hello. I'm Node");
+import http from 'http';
+
+const books = [
+
+    { title: 'Four Hundred Souls: A Community History of African America, 1619-2019', pubYear: '2021', authors: ['Ibram X. Kendi', 'Keisha N. Blain'] }
+
+];
+
+const server = http.createServer(function (request, response) {
+    const url = new URL(request.url, `http://$request.headers.host`);
+
+    if (url.pathname === `/` && url.searchParams.has('pubYear')) {
+        const pubYear = parseInt(url.searchParams.get('pubYear'), 10);
+        const filteredBooks = books.filter(
+            function (book) {
+                return book.pubYear === pubYear;
+            }
+        );
+
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(filteredBooks));
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        response.end('Endpoint not found');
+    }
+
+});
+
+const PORT = 9000;
+server.listen(PORT, function () {
+    console.log(`Server is running at https://localhost:${PORT}`);
+});

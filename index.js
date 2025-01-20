5be2bd0c3e3d4c5cdd90b3153f9cfe2d503217f4
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-01-20 17:11:23 -0500

    create POST request and books in own file

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 
 import http from 'http';
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+const PORT = 9000;
 
-const books = [
 
-    { title: 'Four Hundred Souls: A Community History of African America, 1619-2019', pubYear: '2021', authors: ['Ibram X. Kendi', 'Keisha N. Blain'] }
+const getBooks = function () {
+    try {
+        const data = fs.readFileSync('./books.json', 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error("Error reading books.json:", err);
+        return [];
+    }
+}
+const saveBooks = function (books) {
+    try {
+        fs.writeFileSync('./books.json', JSON.stringify(books, null, 2));
+    } catch (err) {
+        console.error('Error writing to books.json', err);
+    }
+}
 
-];
+const addBook = function (title, pubYear, authors, publisher, category) {
+    const books = getBooks();
+    const newBoook = {
+        id: uuidv4(),
+        title,
+        pubYear,
+        authors,
+        publisher,
+        category
+    };
+    books.push(newBook);
+    saveBooks(books);
+    return newBook;
+}
 
 const server = http.createServer(function (request, response) {
+
     const url = new URL(request.url, `http://${request.headers.host}`);
-    console.log(url);
+
 
     if (url.pathname === `/` && url.searchParams.has('pubYear')) {
+
         const pubYear = url.searchParams.get('pubYear');
+        const books = getBooks();
+
         const filteredBooks = books.filter(
             function (book) {
                 return book.pubYear === pubYear;
@@ -22,6 +56,19 @@ const server = http.createServer(function (request, response) {
         response.writeHead(200, { 'Content-Type': 'application/json' });
         response.end(JSON.stringify(filteredBooks));
         console.log('The request was successfull');
+    } else if (url.pathname === '/' && request.method === 'POST') {
+        let body = '';
+        request.on('data', function (chunk) {
+            body += chunk.toString();
+        });
+
+        request.on('end', function () {
+            const { title, pubYear, authors, publisher, category } = JSON.parse(body);
+            const newBook = addBook(title, pubYear, authors, publisher, category);
+            response.writeHead(201, { 'Content-Type': 'application/json' });
+            response.end(JSON.stringify(newBook));
+            console.log("New Book added:", newBook);
+        });
     } else {
         response.writeHead(404, { 'Content-Type': 'text/plain' });
         response.end('Endpoint not found');
@@ -29,7 +76,7 @@ const server = http.createServer(function (request, response) {
 
 });
 
-const PORT = 9000;
+
 server.listen(PORT, function () {
     console.log(`Server is running at https://localhost:${PORT}`);
 });

79b22d9c959bec47a8b5481e1c0ea9ecfb94d7f5
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-01-31 08:02:10 -0500

    …

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-import http from 'http';
+import http from 'node:http';
 import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
 import path from 'path';
@@ -43,7 +43,6 @@ const server = http.createServer(function (request, response) {
     const url = new URL(request.url, `http://${request.headers.host}`);
 
 
-
     if (url.pathname === `/` && url.searchParams.has('pubYear')) {
 
         const pubYear = url.searchParams.get('pubYear');
@@ -58,6 +57,7 @@ const server = http.createServer(function (request, response) {
         response.writeHead(200, { 'Content-Type': 'application/json' });
         response.end(JSON.stringify(filteredBooks));
         console.log('The request was successfull');
+
     } else if (url.pathname === '/' && request.method === 'POST') {
 
         let body = '';
@@ -67,9 +67,11 @@ const server = http.createServer(function (request, response) {
         request.on('end', function () {
             const { title, pubYear, authors, publisher, category } = JSON.parse(body);
             const newBook = addBook(title, pubYear, authors, publisher, category);
+
             response.writeHead(201, { 'Content-Type': 'application/json' });
             response.end(JSON.stringify(newBook));
             console.log("New Book added:", newBook);
+
         });
     } else if (url.pathname === '/') {
         let filePath = path.join(process.cwd(), 'public', url.pathname === '/' ? 'index.html' : url.pathname);
@@ -99,9 +101,10 @@ const server = http.createServer(function (request, response) {
         response.end('Endpoint not found');
     }
 
+
 });
 
 
 server.listen(PORT, function () {
-    console.log(`Server is running at https://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });

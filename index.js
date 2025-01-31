e641952ee745af871b2e0f14bb189b61bca27c29
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-01-31 14:02:37 -0500

    …

--- index.js
+++ index.js
@@ -43,52 +43,52 @@ const server = http.createServer(function (request, response) {
 
         });
     } else if (url.pathname === '/' && request.method === 'PATCH') {
-        if (url.searchParams.has('id') and url.searchParams.has('bookProperty')) {
+        if (url.searchParams.has('id') && url.searchParams.has('bookProperty')) {
             let body = '';
-request.on('data', function (chunk) {
-    body += chunk.toString();
-});
+            request.on('data', function (chunk) {
+                body += chunk.toString();
+            });
 
-request.on('end', function () {
-    // In this case we're going to assume that the patch will always update the title
-    const { title } = JSON.parse(body);
+            request.on('end', function () {
+                // In this case we're going to assume that the patch will always update the title
+                const { title } = JSON.parse(body);
 
-    updateBook(title, id);
+                updateBook(title, id);
 
-});
+            });
         } else {
-    response.writeHead(404, { 'Content-Type': 'text/plain' });
-    response.end('Endpoint not found');
-}
+            response.writeHead(404, { 'Content-Type': 'text/plain' });
+            response.end('Endpoint not found');
+        }
 
     }
     else if (url.pathname === '/') {
-    let filePath = path.join(process.cwd(), 'public', url.pathname === '/' ? 'index.html' : url.pathname);
-    const ext = path.extname(filePath);
-    console.log(filePath);
-    let contentType = 'text/html';
-    if (ext === '.js') {
-        contentType = 'application/javascript';
-    } else if (ext === '.css') {
-        contentType = 'text/css';
-    } else if (ext === '.json') {
-        contentType = 'application/json';
-    }
-
-    fs.readFile(`${filePath}`, 'utf-8', (err, data) => {
-        if (err) {
-            response.writeHead(404, { 'Content-Type': 'text/plain' });
-            response.end('File not found');
-        } else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(data);
+        let filePath = path.join(process.cwd(), 'public', url.pathname === '/' ? 'index.html' : url.pathname);
+        const ext = path.extname(filePath);
+        console.log(filePath);
+        let contentType = 'text/html';
+        if (ext === '.js') {
+            contentType = 'application/javascript';
+        } else if (ext === '.css') {
+            contentType = 'text/css';
+        } else if (ext === '.json') {
+            contentType = 'application/json';
         }
-    });
 
-} else {
-    response.writeHead(404, { 'Content-Type': 'text/plain' });
-    response.end('Endpoint not found');
-}
+        fs.readFile(`${filePath}`, 'utf-8', (err, data) => {
+            if (err) {
+                response.writeHead(404, { 'Content-Type': 'text/plain' });
+                response.end('File not found');
+            } else {
+                response.writeHead(200, { 'Content-Type': contentType });
+                response.end(data);
+            }
+        });
+
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        response.end('Endpoint not found');
+    }
 
 
 });

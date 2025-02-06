3ae382dd08c70c925f89275085aee943aad01296
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-02-06 11:48:03 -0500

    add two different servers with .env variables

--- index.js
+++ index.js
@@ -1,114 +1,62 @@
 
+import dotenv from 'dotenv';
 import http from 'node:http';
-import path from 'path';
-import { getBooks, addBook } from './helper.js';
 import express from 'express';
-const PORT = 9000;
-import { fileURLToPath } from 'url';
+import fs from 'node:fs';
+import path from 'node:path';
+import { getBooks, addBook } from './helper.js';
+dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const SERVER_TYPE = process.env.SERVER_TYPE;
+const PORT = process.env.PORT;
+
 const app = express();
-//app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 
-const server = http.createServer(function (request, response) {
+if (SERVER_TYPE === 'http') {
 
-    const url = new URL(request.url, `http://${request.headers.host}`);
+    const server = http.createServer(function (request, response) {
+
+        //We're extracting the url information from the request so we can make use of it
+        //later in the function
+        const url = new URL(request.url, `http://${request.headers.host}`);
+        console.log()
+        if (url.pathname === '/') {
+            let filePath = path.join(process.cwd(), 'public', url.pathname === '/' ? 'index.html' : url.pathname);
 
 
-    if (url.pathname === `/` && url.searchParams.has('pubYear')) {
-
-        const pubYear = url.searchParams.get('pubYear');
-        const books = getBooks();
-
-        const filteredBooks = books.filter(
-            function (book) {
-                return book.pubYear === pubYear;
-            }
-        );
-
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.end(JSON.stringify(filteredBooks));
-        console.log('The request was successfull');
-
-    } else if (url.pathname === '/' && request.method === 'POST') {
-
-        let body = '';
-        request.on('data', function (chunk) {
-            body += chunk.toString();
-        });
-        request.on('end', function () {
-            const { title, pubYear, authors, publisher, category } = JSON.parse(body);
-            const newBook = addBook(title, pubYear, authors, publisher, category);
-
-            response.writeHead(201, { 'Content-Type': 'application/json' });
-            response.end(JSON.stringify(newBook));
-            console.log("New Book added:", newBook);
-
-        });
-    }
-    else if (url.pathname === '/' && request.method === 'PATCH') {
-        let body = '';
-        request.on('data', function (chunk) {
-            body += chunk.toString();
-        });
-
-        request.on('end', function () {
-            var parsedBody = JSON.parse(body);
-            /*console.log(typeof parsedBody);*/
-            if (parsedBody.hasOwnProperty("id")) {
-                console.log(parsedBody.id);
-                const books = getBooks();
-
-                var filteredBooks = books.filter(function () {
-                    return books.id == parsedBody.id
-                });
-
-                if (filteredBooks.length != 0) {
-                    console.log(filteredBooks);
+            fs.readFile(filePath, 'utf-8', (err, data) => {
+                if (err) {
+                    response.writeHead(404, { 'Content-Type': 'text/plain' });
+                    response.end('File not found');
                 } else {
-                    console.log("no books match the id");
+                    response.writeHead(200, { 'Content-Type': 'text/html' });
+                    response.end(data);
                 }
-            }
-        });
+            });
+        }
 
-    }
-    else if (url.pathname === '/') {
-
-        app(request, response);
-        // let filePath = path.join(process.cwd(), 'public', url.pathname === '/' ? 'index.html' : url.pathname);
-        // const ext = path.extname(filePath);
-        // console.log(filePath);
-        // let contentType = 'text/html';
-        // if (ext === '.js') {
-        //     contentType = 'application/javascript';
-        // } else if (ext === '.css') {
-        //     contentType = 'text/css';
-        // } else if (ext === '.json') {
-        //     contentType = 'application/json';
-        // }
-
-        // fs.readFile(`${filePath}`, 'utf-8', (err, data) => {
-        //     if (err) {
-        //         response.writeHead(404, { 'Content-Type': 'text/plain' });
-        //         response.end('File not found');
-        //     } else {
-        //         response.writeHead(200, { 'Content-Type': contentType });
-        //         response.end(data);
-        //     }
-        // });
-
-    } else {
-        response.writeHead(404, { 'Content-Type': 'text/plain' });
-        response.end('Endpoint not found');
-    }
+    });
 
 
-});
 
 
-server.listen(PORT, function () {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
+    server.listen(PORT, function () {
+        console.log(`Starting the ${SERVER_TYPE} server`);
+        console.log(`Server is running at http://localhost:${PORT}`);
+    });
+
+} else if (SERVER_TYPE === 'express') {
+
+
+    app.listen(PORT, function () {
+        console.log(`Starting the ${SERVER_TYPE} server`);
+        console.log(`Server is running at http://localhost:${PORT}`);
+    });
+
+}
+
+
+
+
+

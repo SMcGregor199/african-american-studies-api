c264164f1bda5502dfb0203f8f6633bae1ce95a9
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-02-12 07:59:21 -0500

    add GET methods to http and express servers

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const SERVER_TYPE = process.env.SERVER_TYPE;
 const PORT = process.env.PORT;
 
 const app = express();
-//const __dirname = new URL('', import.meta.url).pathname;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 if (SERVER_TYPE === 'http') {
@@ -24,26 +24,35 @@ if (SERVER_TYPE === 'http') {
     const server = http.createServer(function (request, response) {
 
         const url = new URL(request.url, `http://${request.headers.host}`);
-        console.log()
+
         if (url.pathname === '/') {
             let filePath = path.join(process.cwd(), 'public', url.pathname === '/' ? 'index.html' : url.pathname);
-
-
             fs.readFile(filePath, 'utf-8', (err, data) => {
+
                 if (err) {
                     response.writeHead(404, { 'Content-Type': 'text/plain' });
                     response.end('File not found');
                 } else {
+                    let modifiedData = data;
+                    if (url.pathname === '/' && url.searchParams.has("pubYear")) {
+                        const pubYear = url.searchParams.get("pubYear");
+                        const books = getBooks();
+
+                        const filteredBooks = books.filter(
+                            function (book) {
+                                return book.pubYear === pubYear;
+                            }
+                        );
+                        const scriptTag = `<script>window.bookData = ${JSON.stringify(filteredBooks)};</script>`;
+                        modifiedData = data.replace('</body>', `${scriptTag}</body>`);
+                    }
                     response.writeHead(200, { 'Content-Type': 'text/html' });
-                    response.end(data);
+                    response.end(modifiedData);
                 }
             });
+
         }
-
     });
-
-
-
 
     server.listen(PORT, function () {
         console.log(`Starting the ${SERVER_TYPE} server`);
@@ -52,16 +61,40 @@ if (SERVER_TYPE === 'http') {
 
 } else if (SERVER_TYPE === 'express') {
 
-    app.use(express.static(path.join(__dirname, 'express-public')));
 
-    app.get('/', function (request, response) {
-        response.sendFile(
-            path.join(__dirname, 'express-public', 'index.html')
-            //'index.html', { root: __dirname }
+    app.use(function (request, response) {
+        console.log(request.query);
+        let filePath = path.join(__dirname, 'express-public', 'index.html');
 
-        );
-        //response.send("HELLO");
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) {
+                response.status(404).send('File not found');
+            } else {
+                let modifiedData = data;
+
+
+                if (request.query.pubYear) {
+
+                    const pubYear = request.query.pubYear;
+
+                    const books = getBooks();
+
+                    const filteredBooks = books.filter(
+                        function (book) {
+                            return book.pubYear === pubYear;
+                        }
+                    );
+                    const scriptTag = `<script>window.bookData = ${JSON.stringify(filteredBooks)};</script>`;
+                    modifiedData = data.replace('</body>', `${scriptTag}</body>`);
+
+                }
+                response.setHeader('Content-Type', 'text/html');
+                response.send(modifiedData);
+            }
+
+        });
     });
+
 
     app.listen(PORT, function () {
         console.log(`Starting the ${SERVER_TYPE} server`);

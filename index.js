dc6d94aa3c8691407bceb9273e39dcb2b78d5ce6
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-02-18 16:50:39 -0500

    implement ejs template language

--- index.js
+++ index.js
@@ -16,9 +16,15 @@ const SERVER_TYPE = process.env.SERVER_TYPE;
 const PORT = process.env.PORT;
 
 const app = express();
+//set up EJS view engine
+app.set('view engine', 'ejs');
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+
+
 if (SERVER_TYPE === 'http') {
 
     const server = http.createServer(function (request, response) {
@@ -26,7 +32,7 @@ if (SERVER_TYPE === 'http') {
         const url = new URL(request.url, `http://${request.headers.host}`);
 
         if (url.pathname === '/') {
-            let filePath = path.join(process.cwd(), 'public', url.pathname === '/' ? 'index.html' : url.pathname);
+            let filePath = path.join(__dirname, 'public', url.pathname === '/' ? 'index.html' : url.pathname);
             fs.readFile(filePath, 'utf-8', (err, data) => {
 
                 if (err) {
@@ -43,8 +49,10 @@ if (SERVER_TYPE === 'http') {
                                 return book.pubYear === pubYear;
                             }
                         );
+                        /*
                         const scriptTag = `<script>window.bookData = ${JSON.stringify(filteredBooks)};</script>`;
                         modifiedData = data.replace('</body>', `${scriptTag}</body>`);
+                        */
                     }
                     else if (!url.searchParams.has("pubYear") && request.method == "POST") {
                         let body = '';
@@ -58,11 +66,10 @@ if (SERVER_TYPE === 'http') {
                             const { title, pubYear, authors, publisher, category } = JSON.parse(body);
 
                             const newBook = addBook(title, pubYear, authors, publisher, category);
-
+                            /*
                             const scriptTag = `<script>window.bookData = ${JSON.stringify(newBook)};</script>`;
-
                             modifiedData = data.replace('</body>', `${scriptTag}</body>`);
-
+                            */
                         });
 
                     }
@@ -83,15 +90,14 @@ if (SERVER_TYPE === 'http') {
 
 
     app.use(function (request, response) {
-        console.log(request.query);
-        let filePath = path.join(__dirname, 'express-public', 'index.html');
+        let filePath = path.join(__dirname, 'public', 'index.html');
 
         fs.readFile(filePath, 'utf-8', (err, data) => {
             if (err) {
                 response.status(404).send('File not found');
             } else {
-                let modifiedData = data;
-
+                //let modifiedData = data;
+                let compiledBookData = '';
 
                 if (request.query.pubYear) {
 
@@ -104,12 +110,36 @@ if (SERVER_TYPE === 'http') {
                             return book.pubYear === pubYear;
                         }
                     );
+                    compiledBookData = JSON.stringify(filteredBooks);
+                    /*
                     const scriptTag = `<script>window.bookData = ${JSON.stringify(filteredBooks)};</script>`;
                     modifiedData = data.replace('</body>', `${scriptTag}</body>`);
+                    */
+                } else if (request.method == "POST") {
+                    let body = '';
+                    request.on('data', function (chunk) {
+                        body += chunk.toString();
+                    });
+                    request.on('end', function () {
+
+                        const { title, pubYear, authors, publisher, category } = JSON.parse(body);
+
+                        const newBook = addBook(title, pubYear, authors, publisher, category);
+                        compiledBookData = JSON.stringify(newBook);
+                        /*
+                        const scriptTag = `<script>window.bookData = ${JSON.stringify(newBook)};</script>`;
+                        modifiedData = data.replace('</body>', `${scriptTag}</body>`);
+                        */
+
+                    });
+
 
                 }
+
                 response.setHeader('Content-Type', 'text/html');
-                response.send(modifiedData);
+                console.log(compiledBookData);
+                response.render("home", { serverName: SERVER_TYPE, bookData: compiledBookData });
+
             }
 
         });

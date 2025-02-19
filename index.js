b0703b00cace7ab631ece419cdbc260af0e1d190
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-02-19 16:11:31 -0500

    clean up

--- index.js
+++ index.js
@@ -1,67 +1,70 @@
+import express from 'express';
+const app = express();
+app.set('view engine', 'ejs');
 
 import dotenv from 'dotenv';
-import express from 'express';
-
-
-import { getBooks, addBook } from './helper.js';
 dotenv.config();
 
 const SERVER_TYPE = process.env.SERVER_TYPE;
 const PORT = process.env.PORT;
 
-const app = express();
-//set up EJS view engine
-app.set('view engine', 'ejs');
+import { getBooks, addBook } from './helper.js';
 
 
 
 
-    app.use(function (request, response) {
-        
-        let compiledBookData = '';
-
-        if (request.query.pubYear) {
-
-            const pubYear = request.query.pubYear;
-
-            const books = getBooks();
-
-            const filteredBooks = books.filter(
-                function (book) {
-                    return book.pubYear === pubYear;
-                }
-            );
-            compiledBookData = JSON.stringify(filteredBooks);
-
-        } else if (request.method == "POST") {
-            let body = '';
-            request.on('data', function (chunk) {
-                body += chunk.toString();
-            });
-            request.on('end', function () {
-
-                const { title, pubYear, authors, publisher, category } = JSON.parse(body);
-
-                const newBook = addBook(title, pubYear, authors, publisher, category);
-                compiledBookData = JSON.stringify(newBook);
 
 
-            });
 
 
-        }
 
-        response.setHeader('Content-Type', 'text/html');
-        console.log(compiledBookData);
-        response.render("home", { serverName: SERVER_TYPE, bookData: compiledBookData });
+
+app.use(function (request, response) {
     
-    });
+    let compiledBookData = '';
+
+    if (request.query.pubYear) {
+
+        const pubYear = request.query.pubYear;
+
+        const books = getBooks();
+
+        const filteredBooks = books.filter(
+            function (book) {
+                return book.pubYear === pubYear;
+            }
+        );
+        compiledBookData = JSON.stringify(filteredBooks);
+
+    } else if (request.method == "POST") {
+        let body = '';
+        request.on('data', function (chunk) {
+            body += chunk.toString();
+        });
+        request.on('end', function () {
+
+            const { title, pubYear, authors, publisher, category } = JSON.parse(body);
+
+            const newBook = addBook(title, pubYear, authors, publisher, category);
+            compiledBookData = JSON.stringify(newBook);
 
 
-    app.listen(PORT, function () {
-        console.log(`Starting the ${SERVER_TYPE} server`);
-        console.log(`Server is running at http://localhost:${PORT}`);
-    });
+        });
+
+
+    }
+
+    response.setHeader('Content-Type', 'text/html');
+    console.log(compiledBookData);
+    response.render("home", { serverName: SERVER_TYPE, bookData: compiledBookData });
+
+});
+
+
+app.listen(PORT, function () {
+    console.log(`Starting the ${SERVER_TYPE} server`);
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
 
 
 

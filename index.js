e8a7ab78dfa0f8f3d4307d1ea709f57bf9d540ac
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-04-16 15:12:18 -0400

    add figures route

--- index.js
+++ index.js
@@ -5,10 +5,39 @@ import { getAllData } from './helper.js';
 dotenv.config();
 
 const app = express();
+const data = getAllData(); 
 const PORT = process.env.PORT || 9000;
 const SERVER_TYPE = process.env.SERVER_TYPE || 'local';
 
 app.set('view engine', 'ejs');
+
+app.get('/figures/:id',function(req,res){
+    const figureId = req.params.id;
+    const figure = data.figures.find(function(f){ 
+        return f.id === figureId;
+    });
+    if(!figure){
+        return res.status(404).send("figure not found");
+    }
+    const titles = data.titles.filter(function(t){
+        return t.figureId === figureId;
+    });
+    const conceptIds = new Set(titles.flatMap(function(t){
+       return t.concepts;
+    }));
+    const concepts = data.concepts.filter(function(c){
+        return conceptIds.has(c.id);
+    });
+    res.render("figure", { figure,titles,concepts }); 
+});
+app.get('/concepts/:id', (req, res) => {
+    const conceptId = req.params.id;
+    const concept = data.concepts.find(c => c.id === conceptId);
+    if (!concept) {
+        return res.status(404).send("Concept not found");
+    }
+    
+});
 
 app.get('/',function(req,res){
     const data = getAllData();
@@ -17,14 +46,6 @@ app.get('/',function(req,res){
 
 
 
-// app.get('/api/figures', (req, res) => {
-//     try {
-//         const theObject = getSampleData();
-//         res.json([theObject]); // send it back as an array
-//     } catch (err) {
-//         res.status(500).json({ error: 'Unable to load figure data' });
-//     }
-//   });
 
 
 app.use((req, res) => {
